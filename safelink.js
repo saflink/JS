004a2d2b94a00e7b3b21f661060ae83ebd3c4204
saflink <39943005+saflink@users.noreply.github.com>
2018-06-05 20:55:21 +0700

    Update safelink.js

--- safelink.js
+++ safelink.js
@@ -18,7 +18,12 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 {a_to_vh=true;}
 j++;}
 if(a_to_vh==false)
-{var encryptedUrl=Base64.encode(a_to_vi);a_to_ve[i].href="http://www.safelinkdl.site/?link="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+{var encryptedUrl=Base64.encode(a_to_vi);
+var x=Math.floor((Math.random()*2)+ 1);
+var xxx=null;
+if(x=="1"){xxx="choosing-web-hosting.html"}
+if(x=="2"){xxx="how-why-and-where-to-buy-effective.html"}
+if(x=="3"){xxx="product-creation-purchasing-domain-name.html"}a_to_ve[i].href="http://www.lewatsini.club/p/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
